d58916c6677b8edeafb319682668eafa09335721
William Birkett <williebirkett@gmail.com>
2017-08-24 19:30:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ var uppercase ='HELLO!'
 var mixedCase ='I love you, Grandma.'
 
 
-function sayHiToGrandma(string) {
-  if(string === string.toLowerCase())
-    return 'I can\'t hear you!'}
-   else if (string === string.toUpperCase()) {
-    return 'YES INDEED'}
+function sayHiToGrandma(string); {
+  if(string === string.toLowerCase());
+    return 'I can\'t hear you!'};
+   else if (string === string.toUpperCase()); {
+    return 'YES INDEED'};
   else {(string === "I love you.")
-    return 'I love you, too.'}
+    return 'I love you, too.'};
