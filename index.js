c900be046fd11f60d1e23d23d60de366f8bc8b53
William Birkett <williebirkett@gmail.com>
2017-08-24 19:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function sayHiToGrandma(string) {
   if(string === string.toLowerCase()){
     return 'I can\'t hear you!'; }
    else if (string === string.toUpperCase()){
-    return 'YES INDEED';}
+    return 'YES INDEED!';}
   else (string === "I love you.")
     return 'I love you, too.'; };
